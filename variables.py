1cc52b91e65b9963c107411dfbb74eff33636d70
edwinlinCEE <edwin.lin@csedge.org>
2022-07-06 13:47:51 -0700

    commit

--- variables.py
+++ variables.py
@@ -4,6 +4,9 @@
 # -------------------------------------------- 
 
 # Example
+from operator import mod
+
+
 message = "Hello World!"
 print(message)
 
@@ -15,6 +18,9 @@ print(message)
 # -------------------------------------------- 
 
 print("------------------- Challenge 1 -------------------")
+name = "Edwin"
+grade = 12
+funFact = "I'm cool."
 
 print(f"Hi! My name is {name}")
 print(f"I'm in the {grade}th grade.")
@@ -26,23 +32,33 @@ print(f"A fun fact about me is that {funFact}")
 # Hint: Also think about where you would initialize those variables
 
 
+
 # **** Upchallenge! ****
 # Can you also print out the data type of the variables? 
 
-
+print(f"name datatype: {type(name)}")
+print(f"Grade datatype: {type(grade)}")
+print(f"funFact datatype: {type(funFact)}")
 
 
 # **** Challenge 1: Problem 2 ****
 # Create a string for the different lines of the Happy Birthday Song
 # Print out the song using the strings you declared.
+line_1 = "Happy Birthday to you!"
+line_2 = f"Happy Birthday Dear {name}"
 
+print(line_1)
+print(line_1)
+print(line_2)
+print(line_1)
 
 
 
 # **** Challenge 1: Problem 3 ****
 # Print out a string that has brackets in it.
 
-
+string = "hey"
+print("[" + string + "]")
 
 
 # -------------------------------------------- 
@@ -60,32 +76,43 @@ num2 = 10
 # **** Challenge 2: Problem 1 ****
 # Store the sum of num1 and num2 in a variable.
 # Print the sum.
-
+sum = num1 + num2
+print(sum)
 
 # **** Challenge 2: Problem 2 ****
 # Store the difference between sum (above) and 7 in a variable.
 # Print the difference.
-
+difference = sum - 7
+print(difference)
 
 # **** Challenge 2: Problem 3 ****
 # Store the product of the difference and 3 in a variable.
 # Print the product.
 
+product = difference * 3
+print(product)
 
 # **** Challenge 2: Problem 4 ****
 # Store the power of the product squared in a variable.
 # Print the power.
+
+power = product * product
+print(power)
 
 
 # **** Challenge 2: Problem 5 ****
 # Store the quotient of the power divided by 4 in a variable.
 # Print the quotient.
 
+quotient = power/4
+print(quotient)
 
 # **** Challenge 2: Problem 6 ****
 # Store the remainder of the quotient divided by 2 in a variable.
 # Print the remainder.
 
+remainder = quotient % 2
+print(remainder)
 
 # **** Upchallenge! ****
 # Given a variable that stores a number, print the values that come right before and right after.
@@ -93,5 +120,6 @@ num2 = 10
 # Your code should work if the value of the variable is changed.
 
 num = 9
+print(f"{num-1} {num+1}")
 
 # -------------------------------------------- 
