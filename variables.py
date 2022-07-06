0338f36774bf918503b278e15d960c4cf79770b9
edwinlinCE <edwin.lin@csedge.org>
2022-07-06 14:02:44 -0700

    commit

--- variables.py
+++ variables.py
@@ -47,10 +47,8 @@ print(f"funFact datatype: {type(funFact)}")
 line_1 = "Happy Birthday to you!"
 line_2 = f"Happy Birthday Dear {name}"
 
-print(f"{line_1}")
-print(f"{line_1}")
-print(f"{line_2}")
-print(f"{line_1}")
+print(f"{line_1}" + "\n" + f"{line_1}" + "\n" + f"{line_2}" + "\n" + f"{line_1}")
+
 
 
 
